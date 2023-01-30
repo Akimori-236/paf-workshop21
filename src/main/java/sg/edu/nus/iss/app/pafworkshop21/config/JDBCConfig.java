228b6db283946e9134a76dd899a7a6f6d032f119
Wee Seng <wee.bscl@gmail.com>
2023-01-30 19:40:41 +0800

    JDBC Config with app.prop

--- src/main/java/sg/edu/nus/iss/app/pafworkshop21/config/JDBCConfig.java
+++ src/main/java/sg/edu/nus/iss/app/pafworkshop21/config/JDBCConfig.java
@@ -6,18 +6,31 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.beans.factory.annotation.Value;
 
 @Configuration
 public class JDBCConfig {
 
+    @Value("${spring.datasource.driver-class-name}")
+    private String driverClassName;
+  
+    @Value("${spring.datasource.url}")
+    private String url;
+  
+    @Value("${spring.datasource.username}")
+    private String username;
+  
+    @Value("${spring.datasource.password}")
+    private String password;
+  
     @Bean
     public DataSource dataSource() {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://localhost:3306/db_example");
-        dataSource.setUsername("user");
-        dataSource.setPassword("password");
-        return dataSource;
+      DriverManagerDataSource dataSource = new DriverManagerDataSource();
+      dataSource.setDriverClassName(driverClassName);
+      dataSource.setUrl(url);
+      dataSource.setUsername(username);
+      dataSource.setPassword(password);
+      return dataSource;
     }
     
     @Bean
